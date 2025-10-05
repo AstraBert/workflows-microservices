ad591e08fb2b9dc6db1a278f2b0181d33898bcf6
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-10-05 22:33:30 +0200

    chore: init python

--- orders/schema.sql
+++ orders/schema.sql
@@ -1,7 +1,9 @@
 -- Orders table
 CREATE TABLE orders (
     order_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
+    user_name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    phone TEXT NOT NULL,
     order_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     status TEXT NOT NULL CHECK(status IN ('pending', 'processing', 'shipped', 'delivered', 'cancelled')),
     shipping_address TEXT NOT NULL,
